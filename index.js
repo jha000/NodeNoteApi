4d822a50546edf9f257127a3ef118c2b692474a2
Abhishek Jha <jha85561@gmail.com>
2024-01-08 22:53:59 +0530

    remove dig_lib

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const mysql = require('mysql');
 
 app.use(cors());
 app.use(express.json());
@@ -18,53 +17,6 @@ const mediaData = require('./json/getMedia.json');
 const musicSeriesData = require('./json/musicSeries.json');
 const musicSingleData = require('./json/musicSingle.json');
 const recentData = require('./json/recent.json');
-
-
-// MySQL Database Configuration
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Abhi@15012002',
-  database: 'dig_lib',
-});
-
-// Connect to MySQL
-db.connect(err => {
-  if (err) {
-    console.error('MySQL connection failed:', err);
-  } else {
-    console.log('Connected to MySQL database');
-  }
-});
-
-// API Endpoint to Retrieve Catalogues
-app.get('/get_catalogues', (req, res) => {
-  const sql = 'SELECT catalogue_id, catalogue_name FROM catalogue';
-
-  // Query the database
-  db.query(sql, (err, result) => {
-    if (err) {
-      console.error('Error executing MySQL query:', err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-// Endpoint to retrieve books
-app.get('/books', (req, res) => {
-  const booksQuery = 'SELECT title, author, description, subject, publisher, total_pages, publication_year, category_id FROM books';
-
-  db.query(booksQuery, (booksErr, booksResult) => {
-    if (booksErr) {
-      console.error('Error executing books query:', booksErr);
-      res.status(500).json({ error: 'Internal Server Error' });
-    } else {
-      res.json(booksResult);
-    }
-  });
-});
 
 
 app.get('/', (req, res) => {
