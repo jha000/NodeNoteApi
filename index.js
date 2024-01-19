85c508af8c708d9a1cb3196de5fc6034fc587278
Abhishek Jha <jha85561@gmail.com>
2024-01-19 13:53:40 +0530

    securedb

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ app.use(express.json());
 
 // Create MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'Abhi@15012002',
-  database: 'rrrlfdb',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'Abhi@15012002',
+  database: process.env.DB_DATABASE || 'rrrlfdb',
 });
+
 
 // Connect to MySQL
 db.connect(err => {
